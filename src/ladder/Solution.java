8555a102c9e7dc8e1de5a11047cd182ed13908d6
TheRealWalrus <baranyferenc2@gmail.com>
2019-03-18 23:28:57 +0100

    Complete "ladder"

--- src/ladder/Solution.java
+++ src/ladder/Solution.java
@@ -1,9 +1,18 @@
 package ladder;
 
+import java.util.Scanner;
+
 class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int wallHeight = scanner.nextInt();
+        int angle = scanner.nextInt();
+
+        System.out.println(ladderHeight(wallHeight, angle));
+    }
 
     static int ladderHeight(int wallHeight, int angle) {
-        double solution =  wallHeight / Math.sin(Math.toDegrees(angle));
+        double solution =  wallHeight / Math.sin(Math.toRadians(angle));
 
         return (int) Math.ceil(solution);
     }
